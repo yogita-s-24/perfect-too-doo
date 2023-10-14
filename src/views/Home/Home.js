2fcb8472a8b68a895000f5ab299a6f235ea9a976
yogita-s-24 <yogitashete24@gmail.com>
2023-10-14 16:38:26 +0530

    form handling added

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,13 +1,17 @@
-import React from "react";
-import './Home.css'
+import React, { useState } from "react";
+import "./Home.css";
 
 function Home() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [priority, setPriority] = useState("");
+
   return (
     <>
       <div className="container">
         <h1 className="text-center mt-2"> Perfect TO DO </h1>
 
-        <div className="container d-flex justify-content-evenly mt-3">
+        <div className="container d-flex justify-content-evenly flex-wrap mt-3">
           <div>
             <h5 className="text-center">Show List Here</h5>
             <div
@@ -30,12 +34,43 @@ function Home() {
               }}>
               <div className="card-body">
                 <form>
-                    <div className="container">
-                        <input type="text" placeholder="Enter List Title" className="form-control shadow py-4"/>
-                        <input type="text" placeholder="Enter List Description" className="form-control py-4 shadow mt-5"/>
-                        <input type="text" placeholder="Enter List Priority" className="form-control py-4 shadow mt-5"/>
-                        <button className=" btn btn-dark px-5 d-block mt-5 mx-auto" >Add</button>
-                    </div>
+                  <div className="container">
+                    <input
+                      type="text"
+                      placeholder="Enter List Title"
+                      className="form-control shadow py-4"
+                      value={title}
+                      onChange={(e)=>{
+                        setTitle( e.target.value);
+                        console.log(e.target.value)
+                      }}
+                      />
+                      
+                    <input
+                      type="text"
+                      placeholder="Enter List Description"
+                      className="form-control py-4 shadow mt-5"
+                      value={description}
+                      onChange={(e)=>{
+                        setDescription( e.target.value);
+                        console.log(e.target.value)
+                      }}
+                    />
+
+                    <input
+                      type="text"
+                      placeholder="Enter List Priority"
+                      className="form-control py-4 shadow mt-5"
+                      value={priority}
+                      onChange={(e)=>{
+                        setPriority( e.target.value);
+                        console.log(e.target.value)
+                      }}
+                    />
+                    <button className=" btn btn-dark px-5 d-block mt-5 mx-auto">
+                      Add
+                    </button>
+                  </div>
                 </form>
               </div>
             </div>
