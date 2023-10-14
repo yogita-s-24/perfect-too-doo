3fd6ada1fac46e04081c0a54753b3044265f732f
yogita-s-24 <yogitashete24@gmail.com>
2023-10-14 15:44:27 +0530

    installed bootstrap and also imported in home component

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./Home.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min"
 
 export const Home = () => {
   return (
   <>
-    <div>
-        
+    <div >
+
     </div>
   </>
   )
